5a70d30802b20a72d91408f8b226583ca38d8449
JamesSeo <roles3203@naver.com>
2022-08-17 17:09:22 +0900

    Python practice on 2022-08-17 17:09:22

---
+++ save.ps1
@@ -0,0 +1,10 @@
+git init .
+
+git remote add origin https://github.com/SW4GGER23/Python.git
+
+git config --global user.email "roles3203@naver.com"
+git config --global user.name "JamesSeo"
+
+git add .
+git commit -m "Python practice on `date +'%Y-%m-%d %H:%M:%S'`";
+git push -u origin master
