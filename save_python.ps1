cf13110f23818798d3b3d4b268c640ea7d039c05
SW4GGER23 <96808962+SW4GGER23@users.noreply.github.com>
2022-08-21 21:02:05 +0900

    Update save_python.ps1

--- save_python.ps1
+++ save_python.ps1
@@ -1,6 +1,6 @@
 git init .
 
-git remote add origin https://SW4GGER23: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@github.com/SW4GGER23/Python.git
+git remote add origin https://SW4GGER23:ACCESS_CODE@github.com/SW4GGER23/Python.git
 
 git config --global user.email "roles3203@naver.com"
 git config --global user.name "JamesSeo"
